dd09ad57044f43de827c6bbddd97b2acae9db446
blueshark226124 <blueshark226124@gmail.com>
2019-06-27 21:40:17 +1000

    If user doesn't jump over minimum 3 enemies, he can't win

--- php/ajax.php
+++ php/ajax.php
@@ -10,8 +10,13 @@ if ($action == "create") {
 	$date         = (empty($_POST['date'])) ? "" : $_POST['date'];
 	$enemy_count  = (empty($_POST['enemy_count'])) ? "" : $_POST['enemy_count'];
 
+	// create a new record
+	$sql = "INSERT INTO result (username, phone_number, score, `date`) VALUES ('".$username."', '".$phone_number."', '".$score."', '".$date."')";
+	$result = mysqli_query($db, $sql);
+	$id = mysqli_insert_id($db);
+
 	// check if it's the first try or not
-	$sql = "SELECT * FROM result WHERE username = '".$username."' AND phone_number = '".$phone_number."' AND `date` = '".$date."'";
+	$sql = "SELECT * FROM result WHERE username = '".$username."' AND phone_number = '".$phone_number."' AND `date` = '".$date."' AND win = 1";
 	$result = mysqli_query($db, $sql);
 	$rowcount = mysqli_num_rows($result);
 
@@ -20,15 +25,15 @@ if ($action == "create") {
 		$msg = "Thank you for playing! We see you already won today. Redeem your current Wicode first to be able to win again.";
 	} else {
 		if ($enemy_count >= 3) {
+			$sql = "UPDATE result SET win = 1 WHERE id='".$id."'";
+			mysqli_query($db, $sql);
+
 			$msg = "Woohoo! You won! We will sms your prize shortly. If you have not received your coupon due to your network provider, contact Talk To Us or Whatsapp 076 553 6337.";
 		} else {
 			$msg = "Not so lucky this time! Keep playing to WIN MORE & stand a chance to win a BIGGER prize!";
 		}
-		
 	}
 
-	$sql = "INSERT INTO result (username, phone_number, score, `date`) VALUES ('".$username."', '".$phone_number."', '".$score."', '".$date."')";
-	$result = $db->query($sql);
 	$response = array(
 		'status' => true,
 		'msg' => $msg
---
+++ php/database.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `jumpinglion`.`result`   
+	ADD COLUMN `win` TINYINT(1) DEFAULT 0 NOT NULL AFTER `date`;
